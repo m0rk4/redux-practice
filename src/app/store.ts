4682ca9584a9dfe2af02121a06c0a4be28e87374
Mark Putyato <mark_putyato@mail.ru>
2022-04-02 23:50:41 +0300

    Added prepare function in reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,17 +1,14 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {configureStore} from '@reduxjs/toolkit';
 
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+import postsReducer from '../features/posts/postsSlice'
+
+const store = configureStore({
+    reducer: {
+        posts: postsReducer
+    },
 });
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
-export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+
+export default store;
---
+++ src/features/posts/postsSlice.ts
@@ -0,0 +1,45 @@
+import {createSlice, nanoid, PayloadAction} from "@reduxjs/toolkit";
+
+interface Post {
+    id: string;
+    title: string;
+    content: string;
+}
+
+const initialState: Post[] = [
+    {id: '1', title: 'First Post!', content: 'Hello!'},
+    {id: '2', title: 'Second Post', content: 'More text'}
+]
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState,
+    reducers: {
+        postAdded: {
+            reducer(state, action: PayloadAction<Post>) {
+                state.push(action.payload);
+            },
+            prepare(title: string, content: string) {
+                return {
+                    payload: {
+                        id: nanoid(),
+                        title,
+                        content
+                    }
+                };
+            }
+        },
+        postUpdated(state, action: PayloadAction<Post>) {
+            const {id, title, content} = action.payload;
+            const existing = state.find(post => post.id === id);
+            if (existing) {
+                existing.title = title;
+                existing.content = content;
+            }
+        }
+    }
+});
+
+export const {postAdded, postUpdated} = postsSlice.actions;
+
+export default postsSlice.reducer;
